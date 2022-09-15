dc301d2b70d8d7870fadd9cb8adbb2757fe7734d
anitha <contact.nara.anitha@gmail.com>
2022-09-15 16:45:47 -0400

    updated 9/15/22 4:45pm

--- src/test/java/com/org/WebstaurantStore/AddProductToCartAndEmptyCart.java
+++ src/test/java/com/org/WebstaurantStore/AddProductToCartAndEmptyCart.java
@@ -1,3 +1,13 @@
+// Go to https://www.webstaurantstore.com/
+
+//Search for 'stainless work table'.
+
+//Check the search result ensuring every product has the word 'Table' in its title.
+
+//Add the last of found items to Cart.
+
+//Empty Cart.
+
 package com.org.WebstaurantStore;
 
 import java.util.List;
@@ -35,65 +45,68 @@ public class AddProductToCartAndEmptyCart {
 			Reporter.log("Search for stainless work table products");
 			driver.findElement(By.xpath("//input[@id='searchval']")).sendKeys("stainless work table");
 			driver.findElement(By.xpath("//button[@value='Search']")).click();
-				
+			
+			Reporter.log("Checking products through pagination");
 			String pagecount = driver.findElement(By.xpath("//*[@id='paging']/nav/ul/li[7]/a")).getText();
-			System.out.println(pagecount);
-				
+			Reporter.log("Total number pages - " + pagecount);
+			Reporter.log("Product titles that does not contain Table");	
 			int pageno = Integer.valueOf(pagecount);
 			int total_products =0; 
-				
+			
 				for (int p=1; p <= pageno; p++)
 				{
 					
 					WebElement activePage = driver.findElement(By.xpath("//*[@id='paging']/nav/ul/li/a[text()='"+p+"']"));
-					System.out.println("activepage:"+activePage.getText());
 					activePage.click();
-						
+					
 					List<WebElement> products= driver.findElements(By.xpath("//*[@id='ProductBoxContainer']"));
 					int product_count = products.size();
-					System.out.println("No.of products per page"+ product_count);
 					total_products = total_products + product_count; 
 						
-					System.out.println ("total_products" + total_products ); 
-						
 					List<WebElement> titles= driver.findElements(By.xpath("//*[@id='product_listing']/div/div[2]/a"));
-					int titleCount =titles.size();
-					System.out.println(titleCount);
 						for (WebElement title:titles)
 						{
 							String desc = title.getText();
 							if(!desc.equals("plus") && !desc.matches(".*\\bTable\\b.*"))
 							{
-								System.out.println(desc);
+								Reporter.log(desc);
 								
 							}
 						}
 						
 				}
 				WebElement lastProduct = driver.findElement(By.xpath("//*[@id='ProductBoxContainer'][last()]"));
-				System.out.println(lastProduct.getText());
+				System.out.println("last product in last page:"+lastProduct.getText());
+				
 				driver.findElement(By.xpath("//*[@id='product_listing']/div[60]/div[4]/form/div/div/input[2]")).click();
+				
+				Reporter.log("Product added to the cart:"+lastProduct.getText());
 				
 				String subWindow = driver.getWindowHandle();
 				System.out.println("SubWindow"+ subWindow);
 				driver.switchTo().window(subWindow);
-				Thread.sleep(3000);
+				Thread.sleep(1000);
 				
-				String text = driver.findElement(By.xpath("//*[@id='td']/div[11]/div/div/footer/button[2]")).getText();
-				System.out.println(text);
+				//String text = driver.findElement(By.xpath("//*[@id='td']/div[11]/div/div/footer/button[2]")).getText();
+				
 				driver.findElement(By.xpath("//*[@id='td']/div[11]/div/div/footer/button[2]")).click();
+				Thread.sleep(1000);
 				
-				Thread.sleep(3000);
 				driver.findElement(By.xpath("//*[@id='watnotif-wrapper']/div/p/div[2]/div[2]/a[1]")).click();
-				Thread.sleep(3000);
+				Thread.sleep(1000);
 				driver.findElement(By.xpath("//*[@id='main']/div[1]/div/div[1]/div/button")).click();
 				driver.findElement(By.xpath("//*[@id='td']/div[10]/div/div/div/footer/button[1]")).click();
-				
+				Reporter.log("Product removed from the cart");
+			
 		}		
 				
 					
-					
-					
+		
+		@AfterClass
+		public void tearDown() {
+			driver.quit();
+		}	
+			
 	
 		
 
